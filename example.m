079b73678de77456166f1f2cdbaf814b746e0e66
KadenAgrey <33500355+KadenAgrey@users.noreply.github.com>
2018-10-01 10:19:28 -0600

    Initial Code Dump

---
+++ example.m
@@ -0,0 +1,102 @@
+% Example script on how to setup and launch the data explore function
+
+% First generate the main figure to serve as the ui window.
+mainfig = figure;
+mainfig.OuterPosition(1:2) = [30, 50];
+mainfig.OuterPosition(3:4) = mainfig.OuterPosition(3:4)*1.8;
+x = 0:0.1:10;
+
+for plt = 2:4
+    if plt == 3
+        ax = subplot(2,2,[1 plt]);
+    else
+        ax = subplot(2,2,plt);
+    end
+
+    if plt == 2
+        ln1 = plot(x, cos(x), 'o');
+    elseif plt == 3
+        ln1 = plot(x, sin(x), 'o');
+    elseif plt == 4
+        ln1 = plot(x, tan(x), 'o');
+    end
+
+    hold on
+    y2 = (x).^2;
+    ln2 = plot(x, y2/max(y2), 'x');
+    hold off
+
+    title(['Plot ' num2str(plt)])
+    xlabel('x')
+    ylabel('y')
+
+    % In this simple case we have one axes and two lines, however we only want 
+    % the first line to be selectable. To determine which lines will be made
+    % selectable the user must set the 'Tag' property of the axes and line as
+    % 'Explorable'.
+    ln1.Tag = 'Explorable'; % We only choose the sine line. 
+    ax.Tag = 'Explorable'; % This is set to refine the search path. Though the search path should never be large so I should change this.
+end
+
+% exploreResults has an option for display boxes to show information on the
+% selected point. It can get this information from the line selected and/or
+% the user can provide the information in a cell array.
+uispec = {{ 'Selected Index', 1:length(x), 'y2', y2/max(y2) },...
+          { 'y2', y2/max(y2) },...
+          { 'y2', y2/max(y2) }};
+
+% Push button call back
+% This must be a cell array where the first entry is an anonymous function
+% and the following are arguments for that function. See the documentation
+% on this argument for details on the requirements of this function.
+cutoff = 1; % just an argument for the example function
+pbtn_callback = {@ userCallback, ln2, [], cutoff, []};
+
+% Set two aditional options
+usefigdat = true; % use data from figure for display boxes
+linkselect = true; % if there are multiple plots select the same point on all of them (requires each line has the same number of points)
+
+% Finally launch the ui figure
+exploreResults( mainfig, pbtn_callback, uispec, usefigdat, linkselect );
+
+%% --- Example User Function --- %%
+function [ newfig ] = userCallback(src, event, slct, ui, ln, extrapnt, cutoff, newfig)
+% An example of a function to assign to the ui push button. See
+% documentatoin for details on the reserved input arguments for src, event,
+% slct and ui.
+
+% I recomend placing a breakpoint in here and running the script to take a
+% look at the structure of the arguments passed to this function
+
+ind = slct(1).ind; % for this case all indices should be the same
+
+% --- Do something for fun --- %
+if ~isempty(extrapnt) && isvalid(extrapnt)
+    delete(extrapnt);
+end
+% Place a marker on the selected axes
+extrapnt = line(ln.XData(ind), ln.YData(ind), 'LineStyle', 'none', 'Marker', '+', 'MarkerSize', 10);
+
+% If this point on the third plot is below the cutoff
+if abs(slct(3).xplobj.YData(ind)) < cutoff
+    if ~isempty(newfig) && isvalid(newfig)
+        close(newfig);
+    end
+    newfig = figure;
+    surf(160*membrane(1,100), 'EdgeColor', 'none');
+    axis tight
+else
+    disp(['Cutoff is ' num2str(cutoff)])
+end
+
+% --------------------------- %
+
+% We can optionally update the arguments for this callback like so. The 
+% user data should be organized in a cell array as with the initial call to
+% exploreResults. The first three arguments to the callback are set by 
+% exploreResults and normally don't need to be editted here.+
+
+user_args = {@ userCallback, ln, extrapnt, cutoff, newfig};
+src.Callback{end} = user_args;
+
+end
---
+++ exploreResults.m
@@ -0,0 +1,525 @@
+function [ mainplot ] = exploreResults( mainplot, pbtnfcn, txtedtdat, usefigdat, linkselect )
+% exploreResults launches an interactive plot from which data points can be
+% selected and detailed results for that case viewed.
+% By: Kaden Agrey
+% v1.1 2018.07.11
+% 
+% Input
+%   mainplot: This can be a figure handle or anonymous function that
+%   returns a figure (with no input arguments). For the ui features to work
+%   the axes that contain selectable data must have the 'Tag' propery set
+%   to "Explorable". 
+%       mainplot.Children(ax).Tag = 'Explorable' 
+%   Further, the object containing the data that should be selectable 
+%   (such as a line plot) should have the same setting.
+%       mainplot.Children(ax).Children(ln).Tag = 'Explorable'
+%   The these options are set corretly by default
+%       mainplot.Children(ax).Children(ln).HitTest = 'on'
+%       mainplot.Children(ax).Children(ln).PickableParts = 'visible'
+%   The script will automatically add space for the for the ui elements.
+%   TODO: Set fig, axes, and line units carefully
+%   TODO: Allow for 3D data sets to be selected
+%   TODO: Other graphical objects (legends, colorbars) break the script
+% 
+%   pbtnfcn: This argument is used to initialize the push button called
+%   'View Details'. It should be a cell array with an anonymous function
+%   handle as the first element and arguments to the function as later
+%   elements.
+%   The first 4 arguments to this function MUST be reserved for
+%   exploreResults. The latter arguments should correspond to those in the
+%   cell array. 
+%       function argout = myFunc(src, event, slct, ui, arg1, arg2, ... )
+%       src: matlab variable pointing to the src of the callback.
+%       event: matlab variable giving event information
+%       slct: exploreResults variable with information on the selected
+%       points. 
+%           slct(i).xplobj: graphics object with explorable data
+%           slct(i).pnt: list of graphics objects for selected points
+%           slct(i).ind: list of selected linear indices from explorable 
+%           data
+%       ui: exploreResults variable with pointers to all ui elements
+%       created
+%   TODO: Currently only one push button can be set - allow multiple
+% 
+%   txtedtdat: A cell array with name/data pairs that will display
+%   information on the selected points under each axes. The cell array
+%   should be nested so that the first index corresponds to the axes number
+%   and the second to the name/data pairs.
+%       txtedtdat = { {name1, data1, name2, data2}, ... % axes 1
+%                     {name1, data1} }                  % axes 2
+%   Axes numbers should index in the same order that the "Explorable" axes
+%   were created and the data given to each name must have the same number
+%   of elements as the line from which it will be selected.
+% 
+%   usefigdat: Additionally the user can allow the function to
+%   automatically get the data from the axes and line objects by setting 
+%   this option to true. This will create two text/edit pairs for each 
+%   "Explorable" axes.
+% 
+%   linkselect: When this option is set to true it will force all
+%   "Explorable" axes to have the same index selected. Selecting a point on
+%   one plot will select the same point on all plots. 
+%   TODO: Let user specify which plots to link
+% 
+% Change Log
+%   2018.07.31: Added support for multiple rows of subplots
+%   2018.08.15: Added support for plots generated from gridded data (like
+%   contours) via linear index conversion.
+%   2018.09.26: Set unexplorable lines to not be 'pickable'.
+
+%% --- Initialize --- %%
+% Generate or pass on the figure
+if isa(mainplot, 'function_handle')
+    fig = mainplot();
+elseif isgraphics(mainplot,'figure')
+    fig = mainplot;
+else
+    error('The main plot variable must be a figure or an anonymous function that will generate a figure')
+end
+
+% Initialize xplr struct with figure objects and slct struct with graphics 
+% objects to select points from. ui struct is also initialized, each field
+% holds the ui objects by associated graphics object. (push buttons for the
+% figure, text/edit boxes for each axes they are places under etc.
+
+% Get all axes and lines marked explorable
+xplr = struct('ax', [], 'ln', []);
+[xplr.ax, xplr.ln] = getExplorable(fig); 
+
+% Initialize the struct to reference all ui objects. Each field will be an
+% array with numel(ui.field) equal to the number of "Explorable" objects in
+% the corresponding field in xplr. (Eg, one figure, possibly several axes
+% and several lines for each axes).
+ui = struct('fig', [], 'ax', [], 'ln', []);
+
+% Get objects that selected points will be associated with
+slct = struct('pnt', [], 'xplobj', []);
+for p = 1:length(xplr.ln)
+    slct(p).xplobj = xplr.ln(p);
+end
+
+%% --- Prepair Main Figure --- %%
+% --- Fig size and plot positioning ---
+% Resize the figure and position the plots to fit the ui
+nch = length(fig.Children); % number of children in figure
+unit = fig.Units; fig.Units = 'pixels'; % save units to reset after
+
+% Get size of all the figure children
+ax_opos = zeros(nch,4); % axes outer position
+for ch = 1:nch
+    fig.Children(ch).Units = 'pixels';
+    ax_opos(ch,:) = fig.Children(ch).OuterPosition;
+end
+
+% Get the unique row positions and the row each child is in
+nrow = 0; % number of plot rows, also used as row index in loop
+row_pos = []; % position of rows
+ax_rows = zeros(nch,1); % the row each child belongs to
+[~, sortind] = sort(ax_opos(:,2), 'descend'); % descending order is required for the following algorithm.
+for ch = sortind'
+    % A row is 'unique' if it hasn't been indexed yet, or if the vertical
+    % position plus the axes height is less than the current row.
+    if ~ismembertol(ax_opos(ch,2), row_pos) && ( isempty( row_pos ) || all( sum(ax_opos(ch,[2 4]))<row_pos ) )
+        nrow = nrow + 1;
+        row_pos(nrow,1) = ax_opos(ch,2);
+        ax_rows(ch) = nrow;
+    else
+        ax_rows(ch) = nrow;
+    end
+end
+
+% Get rows that require padding
+pad_row = []; % list of rows requiring padding
+for row = 1:nrow
+    rowch = find(row==ax_rows)';
+    for ch = rowch
+
+        % If the child is Explorable then add the row to the pad list
+        ind = fig.Children(ch)==xplr.ax;
+        if any(ind) && ( usefigdat || ~isempty(txtedtdat(ind)) )
+            pad_row(length(pad_row)+1,1) = row;
+            break
+        end
+
+    end
+end
+
+% Using the above data set the total figure padding
+figpad = 30 + 5 + 20*2*length(pad_row);
+
+% Resize figure window
+fig.OuterPosition(2) = 50; % just move to the bottom of the screen
+fig.OuterPosition(4) = fig.OuterPosition(4) + figpad;
+
+% Reposition children
+axpad = 30 + 5;
+for row = nrow:-1:1 % start at bottom row
+    % Add text/edit padding if this row requires it
+    if ismember(row, pad_row)
+        axpad = axpad + 20*2;
+    end
+
+    % Move all the axes of this row accordingly
+    rowch = find(row==ax_rows)';
+    for ch = rowch
+        fig.Children(ch).OuterPosition(2) = ax_opos(ch,2) + axpad;
+        fig.Children(ch).Units = 'normalized';
+    end
+end
+
+% --- Make non-explorable axes children 'unpickable' ---
+% Set the 'PickableParts' property for non-explorable axes children to
+% 'none'.
+% We only need to consider axes that are marked explorable
+for a = 1:length(xplr.ax)
+    for ch = 1:length(xplr.ax(a).Children)
+        if ~ismember(xplr.ax(a).Children(ch), xplr.ln)
+            xplr.ax(a).Children(ch).PickableParts = 'none';
+        end
+    end
+end
+
+%% --- Setup UI --- %%
+if isempty(txtedtdat)
+    txtedtdat = cell(1,length(xplr.ax));
+end
+
+% Make button to view details of selected point
+horz = getLeftChild(fig, 'pixels'); % place in line with farthest left plot
+
+ui.fig.pbtn = uicontrol(fig, 'Style', 'pushbutton',... % make the button
+                             'String', 'View Details',...
+                             'Units', 'pixels',...
+                             'Position', [0 0 150 30]);
+
+ui.fig.pbtn.Position = ui.fig.pbtn.Position + [horz 5 0 0]; % five pixel padding
+ui.fig.pbtn.Units = 'normalized'; % reset units
+
+% Make text and edit objects for each explorable axes
+nax = length(xplr.ax);
+for a = 1:nax
+
+    % Initial text and edit box size and options
+    txtopt = {'Position', [0 0 75 20]};
+    editopt = {'Position', [0 0 75 20], 'String', 'Empty', 'Enable', 'inactive'};
+
+    if usefigdat % user setting to use figure axes data
+        axdat = getAxesData(xplr.ax(a));
+        if isempty(txtedtdat{a})
+            txtedtdat{a} = axdat;
+        else
+            txtedtdat{a} = [axdat, txtedtdat{a}];
+        end
+    end
+
+    % Make the text/edit pair
+    [ui.ax(a).txt, ui.ax(a).edt] = makeValueDispBar(xplr.ax(a), txtedtdat{a}, txtopt, editopt);
+
+end
+
+% Assign user callback function to push button
+ui.fig.pbtn.Callback = {@ viewDetailsCallback, slct, ui, pbtnfcn};
+
+% Assign ButtonDownFcn to selectable objects in figures
+for ob = 1:length(slct)
+    % Pass structs with selected point information (slct) and updatable ui 
+    % objects (ui). Pass index of current selectable object.
+    slct(ob).xplobj.ButtonDownFcn = {@ lnChoosePnt, ob, slct, ui, linkselect};
+end
+
+end
+
+%% --- Functions --- %%
+% --- Getters --- %
+function [axs, lns] = getExplorable(fig)
+% Gets pointers to axes and lines from parent figure marked 'explorable'
+
+ca = 1; cl = 1; % counters
+% Loop over all children of parent figure
+for a = length(fig.Children):-1:1
+% We loop backwards because figure children are in reverse order of
+% creation, and it's likely that the users data is organized in order of
+% creation.
+
+    % If user set 'Tag' field to 'Explorable' add child to axes list
+    if strcmp(fig.Children(a).Tag, 'Explorable')
+
+        axs(ca) = fig.Children(a); % Get axes pointer
+
+        % Loop over all children of parent axes
+        for lnn = length(axs(ca).Children):-1:1 % looping backwards for the same reason
+            % If user set 'Tag' field to 'Explorable' add child to line list
+            if strcmp(axs(ca).Children(lnn).Tag, 'Explorable')
+                lns(cl) = axs(ca).Children(lnn); % Get line pointer
+                cl = cl + 1; % increment line counter
+            end
+        end
+
+        ca = ca + 1; % increment axes counter
+
+    end
+end
+
+end
+
+function [figdat] = getAxesData(ax)
+% Gets explorable (selectable) data and labels from an axes.
+
+c = 1; % counter
+figdat = cell(1,1);
+for n = 1:length(ax.Children) % loop over lines on the axes
+    ln = ax.Children(n);
+    if strcmp(ln.Tag, 'Explorable')
+        % Get x and y data
+        figdat(c:c+3) = {ax.XLabel.String, ln.XData, ax.YLabel.String, ln.YData};
+        c = c+4;
+        % Get z data if present
+        if ~isempty(ax.ZLabel.String)
+            figdat(c:c+1) = {ax.ZLabel.String, ln.ZData};
+            c = c+2;
+        end
+    end
+end
+
+end
+
+function [pos, ind] = getLeftChild(parent, unit)
+% Gets leftmost child object
+nch = length(parent.Children);
+pos = zeros(nch,1);
+for ch = 1:nch
+    oldunit = parent.Children(ch).Units; % to reset units
+    parent.Children(ch).Units = unit; % set units
+
+    pos(ch) = parent.Children(ch).Position(1); % get horizontal position
+
+    parent.Children(ch).Units = oldunit; % reset units
+end
+[pos,ind] = min(pos);
+
+end
+
+function [pos, ind] = getBottomChild(parent)
+% Gets lowest child object
+nch = length(parent.Children);
+pos = zeros(nch,1);
+for ch = 1:nch
+    oldunit = parent.Children(ch).Units; % to reset units
+    parent.Children(ch).Units = unit; % set units
+
+    pos(ch) = parent.Children(ch).Position(2); % get horizontal position
+
+    parent.Children(ch).Units = oldunit; % reset units
+end
+[pos,ind] = min(pos);
+
+end
+
+% --- Makers --- %
+function [txt, edt] = makeValueDispBar(ax, tedat, varargin)
+% Makes a bar of labeled edit fields under the given axes
+
+% Handle optional arguments
+%             { txtopt, editopt }
+default_opt = { {},     {} };
+% Overwrite defaults
+default_opt(1:length(varargin)) = varargin;
+% Assign to pretty variable names
+[txtopt, editopt] = default_opt{:};
+
+% Make text/edit pairs for each axes
+txt = gobjects(1,length(tedat)/2); edt = txt; % preinitialize
+for n = 1:length(tedat)/2
+
+    % Make edit box label
+    if n > 1 % place next to last text box
+        txt(n) = makePairedText(txt(n-1), [txtopt, {'String', tedat{2*n-1}}]);
+    else % else place under axes
+        txt(n) = makePairedText(ax, [txtopt, {'String', tedat{2*n-1}}]);
+    end
+
+    % Make the paired edit box
+    edt(n) = makePairedEdit( txt(n), [editopt, {'Userdata', tedat{2*n}}] );
+
+    % Set units to normalized
+    txt(n).Units = 'normalized'; edt(n).Units = 'normalized';
+
+end
+
+end
+
+function [txt] = makePairedText(anchor, txtopt)
+% Places and sizes initial text box label for edit box to go underneath
+fig = ancestor(anchor,'figure');
+
+% Make text object
+txtset = [{fig, 'Style', 'text', 'Units', 'pixels'}, txtopt];
+txt = uicontrol(txtset{:});
+
+% Set label size and position relative to associated axes and previous
+% labels
+
+chkExtent(txt); % ensure string doesn't leave box
+txt.Position(4) = txt.Extent(4); % remove vertical padding
+
+txt.Units = 'pixels'; 
+tpos = txt.Position;
+
+% If an axes is passed as the anchor set below left corner, if a txt object
+% set beside.
+unit = anchor.Units; anchor.Units = 'pixels';
+if isgraphics(anchor,'axes')
+    vert = anchor.OuterPosition(2) - tpos(4); % set pair below axes
+    horz = anchor.Position(1) + 3; % set pair on left plot edge
+else
+    vert = anchor.Position(2); % set pair at same height as last pair
+    horz = anchor.Position(1) + 3 + anchor.Position(3); % set pair next to last pair
+end
+anchor.Units = unit;
+
+% Finally set the position
+txt.Position = [horz vert tpos(3:4)];
+
+end
+
+function [edt] = makePairedEdit(txt, edtopt)
+% Places an edit box underneath a text field
+fig = ancestor(txt,'figure');
+
+% Make edit box
+editset = [{fig, 'Style', 'edit', 'Units', 'pixels'}, edtopt];
+edt = uicontrol(editset{:});
+
+% Set position of edit box relative to label
+edt.Units = txt.Units;
+tpos = txt.Position;
+epos = edt.Position;
+dif3 = tpos(3) - epos(3); % to keep label and box centred
+edt.Position = [tpos(1)+dif3/2 tpos(2)-epos(4) epos(3:4)];
+
+end
+
+% --- Utility --- %
+function [] = chkExtent(txt)
+% Checks that the extent of the string in a text object does not exceed the
+% size. Because the 'Extent' property doesn't consider automatic string
+% wrap this function doesn't either.
+
+unit = txt.Units;
+txt.Units = 'pixels';
+
+% Width first - get size difference
+dif3 = txt.Extent(3) - txt.Position(3) + 3; % Include some padding
+% Adjust box size accordingly
+if dif3 > 0
+    txt.Position(3) = txt.Position(3) + dif3;
+end
+% Set the height likewise
+dif4 = txt.Extent(4) - txt.Position(4) + 3;
+if dif4 > 0
+    txt.Position(4) = txt.Position(4) + dif4;
+end
+
+txt.Units = unit; % reset units
+
+end
+
+% --- Callbacks --- %
+function [] = lnChoosePnt(src, event, n, slct, ui, lnksel)
+% Assigned as ButtonDownFcn to a line to select nearest point when line is
+% clicked
+%
+% Input
+%   n: the index of the selected object in slct
+%   slct: the select struct defined in the main function. This struct will
+%         hold the index of the selected points
+%   ui: the ui struct defined in the main function
+%   lnksel: the option to link selections on all selectable lines
+
+ax = ancestor(slct(n).xplobj, 'axes'); % Get the parent axes
+fig = ancestor(ax, 'figure'); % Get the parent figure
+
+if strcmp(fig.SelectionType,'normal') % left click?
+
+    % Get position of mouse
+    pos = ax.CurrentPoint(1, 1:2); 
+
+    % Normalize by figure limits before search
+    xn = abs( diff(ax.XLim) ); yn = abs( diff(ax.YLim) );
+    x = slct(n).xplobj.XData./xn;
+    y = slct(n).xplobj.YData./yn;
+    pos = pos./[xn yn];
+
+    % Find nearest data point
+    if isvector(x)
+        ind = dsearchn( [ x; y ]', pos );
+    else
+        % If the XData is an ND matrix then it is gridded data
+        ind = sub2ind(size(x), dsearchn(y(:,1), pos(2)), dsearchn(x(1,:)', pos(1)) );
+    end
+
+    % Update all points when link all selections is on, otherwise just the
+    % selected point.
+    if lnksel
+        slist = 1:length(slct);
+    else
+        slist = n;
+    end
+
+    for s = slist
+        % Set selected index
+        slct(s).ind = ind;
+
+        % Snap to nearest data point
+        p(1) = slct(s).xplobj.XData(ind);
+        p(2) = slct(s).xplobj.YData(ind);
+
+        % Update the graphical line object for the selected point
+        if ~isempty(slct(s).pnt)
+            delete(slct(s).pnt)
+        end
+        sax = ancestor(slct(s).xplobj, 'axes');
+        slct(s).pnt = line(sax, p(1), p(2), 'Marker', 'o', 'MarkerSize', 6, 'MarkerFaceColor', 'm'); % TODO: allow specifying point options
+
+        % Set edit box values
+        for edt = ui.ax(s).edt
+            edt.String = num2str( edt.UserData(ind), 5 );
+        end
+    end
+
+end
+
+% Update structure by updating the ButtonDownFcn arguments
+for s = 1:length(slct)
+    slct(s).xplobj.ButtonDownFcn(2:end) = {s, slct, ui, lnksel};
+    if ~isempty(slct(s).pnt)
+        slct(s).pnt.ButtonDownFcn = {@ lnChoosePnt, s, slct, ui, lnksel}; % let user click on the point too
+    end
+end
+
+% Update slct structure in 'View Details' callback function
+ui.fig.pbtn.Callback{2} = slct;
+
+end
+
+function [] = viewDetailsCallback(src, event, slct, ui, usrcall)
+% Callback function for the view details push button. Takes the currently
+% selected point information and the user supplied anonymous function with
+% arguments. After checking that points have been properly selected will
+% launch the user function with the first three arguments as matlab defined
+% <src>, <event>, and explore_results defined <slct>.
+
+% If points haven't all been selected do nothing.
+if ~isfield(slct, 'ind')
+    return
+end
+for p = 1:length(slct)
+    if isempty(slct(p).ind)
+        return
+    end
+end
+
+% Call the user's function and pass options through
+usrcall{1}( src, event, slct, ui, usrcall{2:end} );
+
+end
