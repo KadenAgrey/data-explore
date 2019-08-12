785e3e6ade86ad283dfa00f0352493612799d79f
Kaden <kbagrey@ucalgary.ca>
2019-08-12 12:06:49 -0600

    Create ex2_ExternalFcnDef

---
+++ ex2_ExternalFcnDef.m
@@ -0,0 +1,29 @@
+%% ex2_ExternalFcnDef
+% ----------------------------------------------------------------------- %
+% This example simply demonstrates that the callback functions do not need
+% to be defined within the same script that exploreResults is called. They
+% can be external as well.
+% ----------------------------------------------------------------------- %
+
+%% Build a figure
+% Just an example plot demonstrate some functionality
+FIG = figure;
+
+x = 0.05:0.05:2;
+y = log(x).^2;
+
+plot(x,y,'-o')
+
+title('Example Plot')
+xlabel('x')
+ylabel('log(x)^2')
+
+%% Setup exploreResults
+% ----------------------------------------------------------------------- %
+% Here we are using a callback function defined in an external function
+% file. Going forward we will keep all callback functions defined within
+% the script that launches exploreResults simply for convenience.
+PUSHBUTTONS = {{'Display', @ex2myFunc}};
+% ----------------------------------------------------------------------- %
+
+exploreResults(FIG, PUSHBUTTONS);
---
+++ ex2myFunc.m
@@ -0,0 +1,8 @@
+function ex2myFunc(src, event, ui, slct)
+% Used in ex2_ExternalFcnDef to demonstrate that the functions used don't
+% need to be defined in the same script that exploreResults is called.
+
+display(slct)
+display(ui)
+
+end
