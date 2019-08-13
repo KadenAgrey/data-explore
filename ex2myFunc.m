0713a7e2dc6cf2999c40bb153ef246986eca97ac
Kaden <kbagrey@ucalgary.ca>
2019-08-13 13:38:23 -0600

    Simplify the external function

--- ex2myFunc.m
+++ ex2myFunc.m
@@ -2,7 +2,6 @@ function ex2myFunc(src, event, ui, slct)
 % Used in ex2_ExternalFcnDef to demonstrate that the functions used don't
 % need to be defined in the same script that exploreResults is called.
 
-display(slct)
-display(ui)
+disp('This is a function')
 
 end
